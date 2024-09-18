3d43a7e3fadb9c72b17b59a5b6d754786f102906
SANTANU <santanumandalx3@gmail.com>
2024-09-18 17:45:57 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By GMIT Electrical Department
+    </footer>
+  )
+}
+
+export default Footer
